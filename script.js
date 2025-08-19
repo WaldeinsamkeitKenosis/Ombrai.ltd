e32faced9a848a4fa5b36cf3e0e94d469e5e64d3
WaldeinsamkeitKenosis <lightconstrukction@gmail.com>
2025-08-19 17:43:52 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 可扩展交互（导航平滑滚动）
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
