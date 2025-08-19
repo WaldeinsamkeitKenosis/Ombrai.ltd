f2be9f493f3811cbdf9572713e2cf80732dd2035
WaldeinsamkeitKenosis <lightconstrukction@gmail.com>
2025-08-19 17:54:17 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// 可扩展交互（导航平滑滚动）
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
